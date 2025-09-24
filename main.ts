75cad841542daec3b62b8026d83f8032acef4e8d
Cifu Asada <148298743+Cifuuuu@users.noreply.github.com>
2025-09-24 15:58:25 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,18 @@
 let x = 0
 let y = 0
 basic.forever(function () {
+    music.setVolume(20)
+    music.play(music.builtinPlayableSoundEffect(soundExpression.soaring), music.PlaybackMode.InBackground)
     x = 2
     y = 4
     for (let index = 0; index < 3; index++) {
         led.toggle(x, y)
-        basic.pause(500)
+        basic.pause(1000)
         led.toggle(x, y)
         y += -1
     }
+    music.stopAllSounds()
+    music.play(music.createSoundExpression(WaveShape.Noise, 4054, 4053, 255, 0, 1344, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
     basic.showLeds(`
         . . . . .
         . . # . .
